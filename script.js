355fd62eb4879109f587c54921ba924f90d9b87b
KhengPC <khengPC@uchiha.com>
2024-03-21 04:23:40 +0700

    complete the project

--- script.js
+++ script.js
@@ -3,3 +3,17 @@ let profileMenu = document.getElementById("profileMenu");
 function toggleMenu() {
   profileMenu.classList.toggle("open-menu");
 }
+
+let sidebarAct = document.getElementById("sidebarAct");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleAct() {
+  sidebarAct.classList.toggle("open-activity");
+  if (sidebarAct.classList.contains("open-activity")) {
+    moreLink.innerHTML = "Show less <b>-</b>";
+  }else{
+    moreLink.innerHTML = "Show more <b>+</b>";
+  }
+}
+
+moreLink.addEventListener("click", toggleAct);
